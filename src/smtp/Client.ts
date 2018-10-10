5d44fb4162161b854e5b7246bf5f25cda12d3a89
xeno <xeno@xeno.yt>
2018-10-11 00:48:49 +0200

    implemented QUIT

--- src/smtp/Client.ts
+++ src/smtp/Client.ts
@@ -8,11 +8,13 @@ export default class SMTPClient {
   private server: SMTPServer;
   private socket: Socket;
   private reader: ReadLine;
+  private fqdn: string;
 
   constructor(server: SMTPServer, socket: Socket) {
     this.server = server;
     this.socket = socket;
     this.reader = createInterface(socket);
+    this.reader.on("line", (line: string) => this._onLine(line));
     this._onConnection();
   }
 
@@ -27,5 +29,26 @@ export default class SMTPClient {
         this.server.getConfig().host + " ESMTP xensmtp"
       )
     );
+  }
+
+  private _onLine(line: string): void {
+    if (line.length === 0) {
+      return;
+    }
+    const splitted = line.split(" ");
+    const header = splitted[0].toUpperCase();
+    switch (header) {
+      case "QUIT":
+        this.write(new SMTPResponse(SMTPResponseCode.Success, "Goodbye!"));
+        this.socket.end();
+        break;
+      default:
+        this.write(
+          new SMTPResponse(
+            SMTPResponseCode.UnimplementedCommand,
+            "Unrecognized command."
+          )
+        );
+    }
   }
 }
